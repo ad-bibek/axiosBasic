c96062fd0463cf9bfbb2ea0c428e719659cd8748
ad-bibek <bibekadhikari988@gmail.com>
2024-05-29 17:44:53 +0545

    created instance

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// AXIOS GLOBALS
+axios.defaults.headers.common['X-Auth-Token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+
 
 // GET REQUEST
 function getTodos() {
@@ -77,7 +81,8 @@ function getTodos() {
             title : 'Hello World'
         },
         transformResponse: axios.defaults.transformResponse.concat(data => {
-            data.title = data.title.toUpperCase(); 
+            data.title = data.title.toUpperCase();
+            return data; 
         })
     };
     axios(options).then(res => showOutput(res));
@@ -85,12 +90,46 @@ function getTodos() {
   
   // ERROR HANDLING
   function errorHandling() {
-    console.log('Error Handling');
+    axios
+    .get('https://jsonplaceholder.typicode.com/todoss')
+    .then(res => showOutput(res))
+    .catch(err => {
+        if(err.response) {
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+
+            if(err. response.status === 404){
+                alert('Error: Page Not Found');
+            }
+        }else if (err.request) {
+            console.error(err.request);
+        }else{
+            console.error(err.message);
+        }
+    });
   }
   
   // CANCEL TOKEN
   function cancelToken() {
-    console.log('Cancel Token');
+    const source = axios.CancelToken.source();
+
+
+    axios
+    .get('https://jsonplaceholder.typicode.com/todos', {
+        cancelToken: source.token
+    })
+    .then(res => showOutput(res))
+    .catch(thrown => {
+        if(axios.isCancel(thrown)){
+            console.log('Request canceled', thrown.message);
+        }
+    });
+
+    if(true){
+        source.cancel('Request Canceled');
+    }
+
   }
   
   // INTERCEPTING REQUESTS & RESPONSES
@@ -105,7 +144,12 @@ function getTodos() {
 
 
   // AXIOS INSTANCES
-  
+  const axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+  });
+  axiosInstance.get('/comments').then(res => showOutput(res));
+
+
   // Show output in browser
   function showOutput(res) {
     document.getElementById('res').innerHTML = `
